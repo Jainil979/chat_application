564bcd13cfbeed0e5ca52ba01976a8aef9a10daf
Jainil979 <jainiljainil39@gmail.com>
2025-12-12 14:26:11 +0530

    Update urls.py

--- chatterly/urls.py
+++ chatterly/urls.py
@@ -7,6 +7,8 @@ from django.urls import path , include
 from accounts.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
 #     # only for development: serve media files through Django
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
